f4b0355e895311efd36d27184579e80b3ef0ff76
Oswin-san <oswindsouzacap@gmail.com>
2025-03-06 17:37:25 +0530

    formatted file

--- mfi_customization/mfi/doctype/company.py
+++ mfi_customization/mfi/doctype/company.py
@@ -1,8 +1,6 @@
 from frappe.utils import validate_email_address
 
 
-
-
 def validate_support_email(doc, method):
     validate_email_address(doc.support_email, True)
     validate_email_address(doc.toner_support_email, True)
